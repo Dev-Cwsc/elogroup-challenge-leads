6de6aff12d778952652b4d10a5be1bb7f5ff3345
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-08 12:51:28 -0300

    Update login.js

--- src/views/pages/login.js
+++ src/views/pages/login.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 function Login() {
-  console.log('está na página de login');
-  return <div><h1>Aqui será a página inicial</h1></div>;
+  return (
+    <div>
+      <h1> Aqui será a página inicial </h1>
+    </div>
+  );
 }
 
 export default Login;
