8210d665faa458ae3af5033a1278687824e2d4e8
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-08 12:23:19 -0300

    Create login.js

---
+++ src/views/pages/login.js
@@ -0,0 +1,10 @@
+function Login() {
+    console.log('está na página de login');
+    return (
+      <div>
+        <h1>Aqui será a página inicial</h1>
+      </div>
+    );
+  }
+  
+export default Login;
