4ed9402c31490f1bab683cbc6930218c756e7a88
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-08 15:52:25 -0300

    Update login.js

--- src/views/pages/login.js
+++ src/views/pages/login.js
@@ -1,9 +1,54 @@
-import React from "react";
+import { useState } from "react";
+import "./css/styles.css";
+import egIMG from "./components/elogroup.png";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1> Aqui será a página inicial </h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title"> Bem vindo </span>
+            <img src={egIMG} alt="EloGroup" />
+            <span className="login-form-title">
+            </span>
+
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Login"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Senha"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta? </span>
+              <a className="txt2" href="/newUser">
+                Criar conta
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
