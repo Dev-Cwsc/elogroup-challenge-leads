b26e34ee75a3d9ce231ba7b341ce3e44abd23d85
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-08 12:51:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/pages/Login"
 import NewUser from "./views/pages/NewUser"
 import Leads from "./views/pages/Leads"
@@ -9,10 +9,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/newUser" element={<NewUser/>}/>
-        <Route path="/leads" element={<Leads/>}/>
-        <Route path="/newLead" element={<NewLead/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/newUser" element={<NewUser />} />
+        <Route path="/leads" element={<Leads />} />
+        <Route path="/newLead" element={<NewLead />} />
       </Routes>
     </Router>
   );
