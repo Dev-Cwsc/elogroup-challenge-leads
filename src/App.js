2fd00e34749a34ee89a083a00fd5679d79989e75
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-08 12:23:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import Routes from './routes';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from "./views/pages/login"
+import NewUser from "./views/pages/newUser"
+import Leads from "./views/pages/leads"
+import NewLead from "./views/pages/newLead"
 
 function App() {
-  return (
-    <>
-      <Routes/> 
-    </>
-  );
+    return(
+    <Router>
+        <Routes>
+            <Route path="/" exact component={<Login />} />
+            <Route path="/newUser" component={NewUser} />
+            <Route path="/leads" component={Leads} />
+            <Route path="/newLead" component={NewLead} />
+        </Routes>
+    </Router>
+    );
 }
-
+ 
 export default App;
