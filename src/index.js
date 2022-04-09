2b6b6d4dcff3fd04695855310cb63137a9830502
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-09 20:29:12 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root'); // Encontra a div com id root no arquivo index.html
+const root = createRoot(container); // Cria um elemento root para o App
 
-root.render(
+root.render( // Renderiza o App
   <React.StrictMode>
     <App />
   </React.StrictMode>,
