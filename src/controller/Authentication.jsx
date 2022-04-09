86a9f1cea1b1cc522c29c5dfa57537042359eaa6
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-09 20:29:14 -0300

    Create Authentication.jsx

---
+++ src/controller/Authentication.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router';
+import Login from '../views/pages/Login'; 
+
+const isAuth = () => { // Retorna o estado de autenticação
+    return false;
+};
+
+const Authentication = () => { // Verifica se o usuário está autenticado
+    const auth = isAuth();
+    return auth ? <Outlet /> : <Login />; // Se estiver autenticado, retorna o componente principal, caso contrário, retorna o componente de login
+};
+
+export default Authentication;
