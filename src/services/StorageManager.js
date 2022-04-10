4c50a9e941bc9bec15f23f1a4ec46afaee299fdb
Carlos Willian Silva <45274089+Dev-Cwsc@users.noreply.github.com>
2022-04-10 11:49:15 -0300

    Update StorageManager.js

--- src/services/StorageManager.js
+++ src/services/StorageManager.js
@@ -1,7 +1,16 @@
-function storeLS(key, value) {
-    
+class StorageManager {
+    static readLS (key) {
+        return JSON.parse(localStorage.getItem(key)); // JSON.parse interpreta o JSON e transforma em objeto
+    }
+
+    static storeLS (key, value) {
+        let data = [];
+        if (this.readLS(key)) {
+            data = JSON.parse(localStorage.getItem(key));
+        }
+        data.push(value);
+        localStorage.setItem(key, JSON.stringify(data)); // JSON.stringfy converte objeto em string
+    }
 }
 
-function readLS(key) {
-    
-}
+export default StorageManager;
